b0c9d22772c5706b4b8557f3bfba171dd4c8dfc3
Diego Edenilson Martinez Lopez <ml24002@esfe.agape.edu.sv>
2024-05-15 07:58:51 -0600

    se actulizo el repositorio

--- src/App.java
+++ src/App.java
@@ -1,5 +1,19 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num1, num2, suma = 0, contador = 0;
+        
+        System.out.print("Ingresa el primer número: ");
+        num1 = scanner.nextInt();
+        
+        System.out.print("Ingresa el segundo número: ");
+        num2 = scanner.nextInt();
+        
+        while (contador < num2) {
+            suma += num1;
+            contador++;
+        }
+        
+        System.out.println("La suma de " + num1 + " repetida " + num2 + " veces es: " + suma);
     }
 }
